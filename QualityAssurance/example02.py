8b180ef803182b6962eeef7934f19001bfbb033d
rideryoyoman <44371345+rideryoyoman@users.noreply.github.com>
2019-10-15 22:43:48 +0800

    Add files via upload

---
+++ QualityAssurance/example02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+"Place module docstring here"
+FIRST = 1
+SECOND = 2
+THIRD = 3
+print(FIRST)
+print(SECOND)
+print(THIRD)
+
+
+# In[ ]:
+
+
+
+
